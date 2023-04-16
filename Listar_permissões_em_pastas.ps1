5ad7438ed6f5aab2629d1f78ca3ec17d6f140781
Joaquim S. M. Junior <35612855+jsalesjunior@users.noreply.github.com>
2023-04-16 14:18:53 -0300

    Listar permissões em pastas compartilhadas.

--- Listar_permissões_em_pastas.ps1
+++ Listar_permissões_em_pastas.ps1
@@ -1,4 +1,5 @@
 # LISTAR PERMISSÕES EM PASTAS
+
 $pasta = "\\Server\compartilhamneto"
 Get-Acl -Path $pasta | Select-Object -ExpandProperty Access |
     Select-Object @{Name=$pasta; Expression={$pasta}}, * | ft -a >> d:\temp\Listagem_permissoes_pasta.txt
